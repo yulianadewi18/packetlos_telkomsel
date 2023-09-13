c2e937461686679ed01323b3c6e15c68e9a5076b
pujirahayu01 <puji89503@gmail.com>
2023-09-13 11:06:38 +0700

    extension datatable

--- app/Views/admin.php
+++ app/Views/admin.php
@@ -1,5 +1,8 @@
 <?= $this->extend('layout/template'); ?>
 <?= $this->section('content'); ?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap4.min.css">
+
 <div id="layoutSidenav_content">
     <main>
         <div class="container-fluid px-4 ">
@@ -158,6 +161,11 @@
         </div>
     </div>
 <?php endforeach; ?>
+
+<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap4.min.js"></script>
+
 
 <script>
     new DataTable('#myTable');
