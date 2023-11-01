404e021220b1875afc40d8c9c940d92ea0e31d09
Desydyn <desynikendhyantiputri@gmail.com>
2023-11-01 08:54:02 +0700

    db

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'db_telkomsel',
+        'database' => 'telkomsel',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
