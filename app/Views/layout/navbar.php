d79a1588a0ba25f5f49b9e9d3b95acfdf5b5d90b
Desydyn <desynikendhyantiputri@gmail.com>
2024-01-03 21:29:37 +0700

    hapus index

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -3,7 +3,7 @@ $session = session();
 ?>
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-danger">
     <!-- Navbar Brand-->
-    <a class="navbar-brand ps-3" href="index.html">Krawung Telkomsel</a>
+    <a class="navbar-brand ps-3" href="index.html">Krawu Telkomsel</a>
     <!-- Sidebar Toggle-->
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href=""><i class="fas fa-bars"></i></button>
 
